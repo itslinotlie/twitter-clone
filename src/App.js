3f65a0288b3e261035f9a9807261529d4f65644a
Michael Li <michael.li.web@gmail.com>
2020-09-09 10:04:35 -0400

    Allocate spacing for sidebar, feed, and widget components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Sidebar from './Sidebar'
-import './App.css';
+import React from "react";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widget from "./Widget";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone</h1>
       <Sidebar/>
-      {/* Insert Feed */}
-      {/* Insert Widgets */}
+      <Feed/>
+      <Widget/>
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Feed.css";
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+            {/* Header */}
+           {/* TweetBox */}
+           {/* Tweets */}
+
+        </div>
+    )
+}
+
+export default Feed
